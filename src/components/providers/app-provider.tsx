36acf7233b20a2b359a76c586501e37ce0b4db7d
peterxyjz <huyq09@gmail.com>
2025-06-21 17:02:07 +0700

    init base project

---
+++ src/components/providers/app-provider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { create } from "zustand";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+});
+
+type AppStoreType = {
+  isAuth: boolean;
+};
+
+export const useAppStore = create<AppStoreType>((set, get) => ({
+  isAuth: false,
+}));
+
+export default function AppProvider(
+  { children }: { children: React.ReactNode }
+) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
